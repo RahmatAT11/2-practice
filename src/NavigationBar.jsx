453bf1fbbb46b187239a86b51d441f5dfdd55cc8
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2022-08-22 02:27:22 -0700

    Change menu A destination to homepage "/"

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,20 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function NavigationBar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/menu-a">Ini A</Link>
-                </li>
-                <li>
-                    <Link to="/menu-b">Ini B</Link>
-                </li>
-                <li>
-                    <Link to="/menu-c">Ini C</Link>
-                </li>
-            </ul>
-        </nav>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Ini A</Link>
+                    </li>
+                    <li>
+                        <Link to="/menu-b">Ini B</Link>
+                    </li>
+                    <li>
+                        <Link to="/menu-c">Ini C</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet />
+        </div>
     );
 }
 
