88ee5773cd6e05ab40540d8db9cdc09320ee4d1f
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2022-08-22 02:26:36 -0700

    Add route for each menu

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import A from './routes/A';
+import B from './routes/B';
+import C from './routes/C';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<A />}/>
+        <Route path="menu-b" element={<B />}/>
+        <Route path="menu-c" element={<C />}/>
+      </Route>
+    </Routes>
   </BrowserRouter>
 );
