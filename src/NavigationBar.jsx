106daf2d48c127544fa4182c102731c75d0cfb4f
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2022-08-22 01:45:37 -0700

    Add three links to three different menu

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+
 function NavigationBar() {
-    return <p>Ini NavBar</p>;
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/menu-a">Ini A</Link>
+                </li>
+                <li>
+                    <Link to="/menu-b">Ini B</Link>
+                </li>
+                <li>
+                    <Link to="/menu-c">Ini C</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default NavigationBar;
